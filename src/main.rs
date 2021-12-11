48f88af8c0ec8844239c94a4f39311ff775a2b67
Janek <development@superyu.xyz>
2021-12-12 00:45:02 +0100

    Update
    Fully added menu and multithreading communication between menu thread and main thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::error::Error;
 
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use memflow_win32::Keyboard;
+use std::thread;
 
 use crate::menu::{Config, handle_menu};
 mod sdk;
 mod cheat;
 pub mod menu;
+
+static mut CONFIG: Config = Config { 
+    aimbot_enable: false,
+    aimbot_fov: 6f32,
+    aimbot_smoothing: 10f32,
+    recoil_control_amount: 30f32,
+    recoil_control_enable: false,
+    glow_enable: false,
+    radar_enable: false,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Starting project membreak...");
@@ -18,29 +30,54 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let keyboard = Keyboard::try_with(&mut kernel).unwrap();
 
-    let config = Config { 
-        aimbot_enable: true,
-        aimbot_fov: 4f32,
-        aimbot_smoothing: 8f32,
-        recoil_control_amount: 0f32,
-        recoil_control_enable: false,
-        glow_enable: false,
-        radar_enable: true,
-    };
+    let mut config = unsafe { CONFIG.clone() };
 
     println!("Cheat setup successful!");
     println!("Starting loop now...");
-    
+
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let menu_thread = thread::spawn(move || {
+        enable_raw_mode().unwrap();
+        loop {
+            let result = handle_menu(&mut config).unwrap();
+
+            if result == false {
+                tx.send(Err("menu quit")).unwrap();
+                disable_raw_mode().unwrap();
+                break;
+            }
+
+            tx.send(Ok(config.clone())).unwrap();
+            thread::sleep(std::time::Duration::from_millis(200));
+        }
+    });
+
     loop {
         unsafe { 
-            let hackloop_success = cheat::hack_loop(&mut  process, client_base, engine_base, &keyboard, &offsets, &config); 
+            let cfg = CONFIG.clone();
+            let new_cfg = rx.recv_timeout(std::time::Duration::from_nanos(500));
+
+            if new_cfg.is_ok() {
+                let new_cfg = new_cfg.unwrap();
+                if new_cfg.is_ok() {
+                    let cfg = new_cfg.unwrap();
+                    CONFIG = cfg;
+                } else {
+                    break;
+                }
+            }
+
+
+            let hackloop_success = cheat::hack_loop(&mut  process, client_base, engine_base, &keyboard, &offsets, &cfg); 
             if !hackloop_success {
                 break;
             }
         };
 
-        std::thread::sleep(std::time::Duration::from_millis(1));
+        thread::sleep(std::time::Duration::from_nanos(500));
     }
 
+    menu_thread.join().unwrap();
     Ok(())
 }
--- src/menu/mod.rs
+++ src/menu/mod.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, io::stdout};
+use std::{error::Error, io::{stdout}};
 
-use crossterm::{style::{SetForegroundColor, SetBackgroundColor, ResetColor, Color, Print}, ExecutableCommand, terminal::{Clear, ClearType}};
+use crossterm::{ExecutableCommand, terminal::{Clear, ClearType}, event::{Event, read, poll, KeyEvent, KeyCode}};
+use std::io::Write;
 
 use serde::{Deserialize, Serialize};
 
@@ -15,35 +16,98 @@ pub struct Config {
     pub recoil_control_amount: f32
 }
 
-static CURRENT_SELECTED: usize = 0;
+static mut CURRENT_SELECTED: i8 = 0;
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-fn draw_checkbox(name: &str, value: &bool, id: &mut usize) {
-    println!("{} - {}", name, value);
+fn draw_checkbox(name: &str, value: &mut bool, id: &mut i8, input_code: usize) {
+
+    if (input_code == 3 || input_code == 4) && unsafe { CURRENT_SELECTED == *id }{
+        *value = !*value;
+    }
+
+    let prefix: &str = unsafe { if CURRENT_SELECTED == *id { "->" } else { "  " } };
+
+    print!("{} {} - {}\n\r", prefix, name, value);
     *id += 1;
 }
 
-fn draw_slider(name: &str, value: &f32, id: &mut usize, start: f32, end: f32, step: f32) {
-    println!("{} - {}", name, value);
+fn draw_slider(name: &str, value: &mut f32, id: &mut i8, start: f32, end: f32, step: f32, suffix: &str, input_code: usize) {
+
+    if input_code == 3 && unsafe { CURRENT_SELECTED == *id } {
+        if *value + step <= end {
+            *value += step;
+        }
+    } else if input_code == 4 && unsafe { CURRENT_SELECTED == *id } {
+        if *value - step >= start {
+            *value -= step;
+        }
+    }
+
+    let prefix: &str = unsafe { if CURRENT_SELECTED == *id { "->" } else { "  " } };
+
+    print!("{} {} - {}{}\n\r", prefix, name, value, suffix);
     *id += 1;
 }
 
-pub fn handle_menu(config: &Config) -> Result<(), Box<dyn Error>> {
+pub fn handle_menu(config: &mut Config) -> Result<bool, Box<dyn Error>> {
 
     let mut id = 0;
+    let mut input_code: usize = 0; 
+    // 0 - nothing
+    // 1 - go up
+    // 2 - go down
+    // 3 - increase or swap bool
+    // 4 - decrease or swap bool
 
-    stdout()
+    if poll(std::time::Duration::from_micros(500))? {
+        let event: Result<KeyEvent, bool> = match read()? {
+            Event::Key(event) => Ok(event),
+            _ => Err(true)
+        };
+
+        if event.is_ok() {
+            let event: KeyEvent = event.unwrap();
+
+            match event.code {
+                KeyCode::Char('q') => { return Ok(false) }
+                KeyCode::Down => unsafe { 
+                    if CURRENT_SELECTED + 1 <= 6 {
+                        CURRENT_SELECTED += 1
+                    }
+                }
+                KeyCode::Up => unsafe { 
+                    if CURRENT_SELECTED - 1 >= 0 {
+                        CURRENT_SELECTED -= 1
+                    }
+                }
+                KeyCode::Right => { input_code = 3 }
+                KeyCode::Left => { input_code = 4 }
+                _ => {}
+            }
+        }
+    }
+
+    let mut out = stdout();
+
+    out
         .execute(Clear(ClearType::All)).unwrap();
 
-    draw_checkbox("Glow", &config.glow_enable, &mut id);
-    draw_checkbox("Radar", &config.radar_enable, &mut id);
+    print!("Project Membreak {} | made with ♡ by superyu\n\r", VERSION);
+    print!("===============================================\n\r");
+    draw_checkbox("Glow", &mut config.glow_enable, &mut id, input_code);
+    draw_checkbox("Radar", &mut config.radar_enable, &mut id, input_code);
 
-    draw_checkbox("Aimbot", &config.aimbot_enable, &mut id);
-    draw_slider("Aimbot FOV", &config.aimbot_fov, &mut id, 0f32, 15f32, 0.25f32);
-    draw_slider("Aimbot Smoothing", &config.aimbot_smoothing, &mut id, 0f32, 15f32, 0.25f32);
+    draw_checkbox("Aimbot", &mut config.aimbot_enable, &mut id, input_code);
+    draw_slider("Aimbot FOV", &mut config.aimbot_fov, &mut id, 0f32, 15f32, 0.25f32, "°", input_code);
+    draw_slider("Aimbot Smoothing", &mut config.aimbot_smoothing, &mut id, 0f32, 15f32, 0.25f32, "",input_code);
 
-    draw_checkbox("Recoil Control System", &config.recoil_control_enable, &mut id);
-    draw_slider("Recoil Control Amount", &config.recoil_control_amount, &mut id, 0f32, 100f32, 5f32);
+    draw_checkbox("Recoil Control System", &mut config.recoil_control_enable, &mut id, input_code);
+    draw_slider("Recoil Control Amount", &mut config.recoil_control_amount, &mut id, 0f32, 100f32, 5f32, "%", input_code);
+    print!("==============================================\n\r");
+    print!("Press q to quit.\n\r");
+    print!("==============================================\n\r");
 
+    out.flush().unwrap();
 
-    Ok(())
+    Ok(true)
 }
